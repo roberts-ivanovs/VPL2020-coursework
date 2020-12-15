3b148854f68143f51c3273aa357b1d6ee245e439
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-12-15 23:32:30 +0200

    Enityt: Created basic `HealthyEntity`

---
+++ core/HealthyEntity.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Numerics;
+
+namespace coursework
+{
+    class HealthyEntity : AbstractEntity
+    {
+
+        private ushort age;
+        private ushort timesGottenSick;
+        private ushort health;
+
+        public HealthyEntity()
+        {
+            health = 100;
+            timesGottenSick = 0;
+            age = 50;  // TODO make the age random in a range
+        }
+    }
+}
