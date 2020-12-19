64718cd94bf6b648b4863c90936732bee801c207
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-12-19 23:49:35 +0200

    Entity: SickEntity will have an initial recovery rate

--- core/Entities/SickEntity.cs
+++ core/Entities/SickEntity.cs
@@ -5,10 +5,11 @@ namespace DiseaseCore
 {
     class SickEntity : HealthyEntity
     {
-        private float recovery = 0;
+        private float recovery = 0f; // 0.0-1.0 | 1.0 being recovered
         private float recoveryRatePerSecond = 1;
         public SickEntity()
         {
+            recoveryRatePerSecond = 1 / this.age;
         }
         override public void Tick(ulong milliseconds)
         {
