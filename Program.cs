16a7a4add295ab5da8e800aea37f29d4b27fb7fb
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-12-20 19:26:38 +0200

    Main: CLI simulation will auto-stop all tasks

--- Program.cs
+++ Program.cs
@@ -10,8 +10,12 @@ namespace coursework
         {
             Console.WriteLine("Hello World!");
             World simulation = new World(95, 5, 1.0f);
+            Console.WriteLine("Starting!");
             simulation.Start();
-            Thread.Sleep(10000);
+            Thread.Sleep(1000);
+            Console.WriteLine("Stopping!");
+            simulation.Stop();
+            Thread.Sleep(1000);
         }
     }
 }
