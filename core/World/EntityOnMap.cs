211995b2ea9439d1ae32d5af0194cf12ff5810ae
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-12-20 21:33:58 +0200

    Entity: EntityOnMap composite will be a public class

--- core/World/EntityOnMap.cs
+++ core/World/EntityOnMap.cs
@@ -3,14 +3,14 @@ using System.Drawing;
 namespace DiseaseCore
 {
 
-    internal class EntityOnMap
+    public class EntityOnMap
     {
         public ulong ID { get; }
         public static ulong IDCounter = 0;
         public Point location;
-        public AbstractEntity entity;
+        internal AbstractEntity entity;
 
-        public EntityOnMap(Point location, AbstractEntity entity)
+        internal EntityOnMap(Point location, AbstractEntity entity)
         {
             IDCounter += 1;
             ID = IDCounter;
