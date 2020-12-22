15f40825c9352d2a674ce9e1ea065b17387ec959
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-12-23 00:21:46 +0200

    Frotnend: Added `DiseaseCore` references to the project

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using DiseaseCore;
 
 namespace frontend
 {
@@ -18,7 +19,8 @@ namespace frontend
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddSingleton<World>();
+            builder.Services.AddSingleton<EntityOnMap>();
             await builder.Build().RunAsync();
         }
     }
