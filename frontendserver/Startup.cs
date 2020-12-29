dbc43f70696f745613df0a8257fb294b21cc8178
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-12-29 20:18:57 +0200

    Frontend: Removal of unused components

--- frontendserver/Startup.cs
+++ frontendserver/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using frontendserver.Data;
 using DiseaseCore;
 
 namespace frontendserver
@@ -29,9 +28,6 @@ namespace frontendserver
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
-            // services.AddSingleton<World>();
-            // services.AddSingleton<EntityOnMap>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
