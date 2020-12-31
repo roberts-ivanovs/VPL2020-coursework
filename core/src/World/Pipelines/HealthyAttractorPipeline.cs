0ceb92dab4eb742e6056a2d5c06e270b4978ce96
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-12-31 14:22:57 +0200

    Simulation: Attractor pipeline supports multithreading

--- core/src/World/Pipelines/HealthyAttractorPipeline.cs
+++ core/src/World/Pipelines/HealthyAttractorPipeline.cs
@@ -4,16 +4,18 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Numerics;
+using System.Threading;
 
 namespace DiseaseCore
 {
 
-    internal class HealthyAttractorPipeline : AbstractPipeline
+    public class HealthyAttractorPipeline : AbstractPipeline
     {
 
         private Random random = new Random();
         private List<Point> attractors = new List<Point>();
 
+        private Mutex dictionaryMutex = new Mutex();
         // Key -> EntityOnMap ID; Value -> index of the next attractor
         private Dictionary<ulong, int> nextAttractor = new Dictionary<ulong, int>();
         private int attractorRadius = World.MaxCoords.X / 300;
@@ -29,41 +31,38 @@ namespace DiseaseCore
         }
         public override PipelineReturnData pushThrough(List<EntityOnMap> currentSick, List<EntityOnMap> currentHealthy, ulong timeDeltaMs)
         {
-            // Random random = new Random();
 
-            // Perform attractor assignment
-            currentHealthy.ForEach(x =>
+            if (dictionaryMutex.WaitOne(5))
             {
-                // Check if is registered
-                if (nextAttractor.ContainsKey(x.ID))
+                // Perform attractor assignment
+                currentHealthy.ForEach(x =>
                 {
-                    var attractorIndex = nextAttractor[x.ID];
-                    // Check if located at desired attractor
-                    if (EntityOnMap.IsIntersecting(x.location, 1, attractors[attractorIndex], (ushort)attractorRadius))
+                    // Check if is registered
+                    if (nextAttractor.ContainsKey(x.ID))
+                    {
+                        var attractorIndex = nextAttractor[x.ID];
+                        // Check if located at desired attractor
+                        if (EntityOnMap.IsIntersecting(x.location, 1, attractors[attractorIndex], (ushort)attractorRadius))
+                        {
+                            // Generte a new attractor to head towards
+                            nextAttractor[x.ID] = random.Next(0, attractors.Count() - 1);
+                        }
+                        // Add some randomness
+                        if (random.Next(100) == 0)
+                        {
+                            var attractorDirectionX = Math.Sign(attractors[attractorIndex].X - x.location.X);
+                            var attractorDirectionY = Math.Sign(attractors[attractorIndex].Y - x.location.Y);
+                            x.entity.direction = new Vector3(attractorDirectionX, attractorDirectionY, 0);
+                        }
+                    }
+                    else
                     {
                         // Generte a new attractor to head towards
                         nextAttractor[x.ID] = random.Next(0, attractors.Count() - 1);
                     }
-                }
-                else
-                {
-                    // Generte a new attractor to head towards
-                    nextAttractor[x.ID] = random.Next(0, attractors.Count() - 1);
-                }
-            });
-
-            // perform direction alterations
-            currentHealthy.ForEach(x =>
-            {
-                // Add some randomness
-                if (random.Next(500) == 0)
-                {
-                    var attractorIndex = nextAttractor[x.ID];
-                    var closestX = Math.Sign(attractors[attractorIndex].X - x.location.X);
-                    var closestY = Math.Sign(attractors[attractorIndex].Y - x.location.Y);
-                    x.entity.direction = new Vector3(closestX, closestY, 0);
-                }
-            });
+                });
+                dictionaryMutex.ReleaseMutex();
+            }
             return new PipelineReturnData
             {
                 newHealthy = currentHealthy,
