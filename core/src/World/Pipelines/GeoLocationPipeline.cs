5d9759d101702b1de08e75ef1db4f667eb687e63
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-12-31 11:39:02 +0200

    Simulation: location update pipeline; code cleanup

--- core/src/World/Pipelines/GeoLocationPipeline.cs
+++ core/src/World/Pipelines/GeoLocationPipeline.cs
@@ -11,28 +11,25 @@ namespace DiseaseCore
         public override PipelineReturnData pushThrough(List<EntityOnMap> currentSick, List<EntityOnMap> currentHealthy, ulong timeDeltaMs)
         {
             var scaledTime = (ulong)(timeDeltaMs * timeScale);
-            // TODO move lambda block to a separate function
-            currentSick.ForEach(x =>
-            {
-                var velocity = x.entity.direction * scaledTime;
-                x.location.X += (int)velocity.X;
-                x.location.Y += (int)velocity.Y;
-                x.location.Y = Math.Min(Math.Max(x.location.Y, 1), World.MaxCoords.Y - 1);
-                x.location.X = Math.Min(Math.Max(x.location.X, 1),  World.MaxCoords.X - 1);
-            });
-            currentHealthy.ForEach(x =>
-            {
-                var velocity = x.entity.direction * scaledTime;
-                x.location.X += (int)velocity.X;
-                x.location.Y += (int)velocity.Y;
-                x.location.Y = Math.Min(Math.Max(x.location.Y, 1), World.MaxCoords.Y - 1);
-                x.location.X = Math.Min(Math.Max(x.location.X, 1),  World.MaxCoords.X - 1);
-            });
+            GeoLocationPipeline.updateLocation(currentSick, scaledTime);
+            GeoLocationPipeline.updateLocation(currentHealthy, scaledTime);
             return new PipelineReturnData
             {
                 newHealthy = currentHealthy,
                 newSick = currentSick,
             };
+        }
+
+        private static void updateLocation(List<EntityOnMap> items, ulong scaledTime)
+        {
+            items.ForEach(x =>
+            {
+                var velocity = x.entity.direction * scaledTime;
+                x.location.X += (int)velocity.X;
+                x.location.Y += (int)velocity.Y;
+                x.location.Y = Math.Min(Math.Max(x.location.Y, 1), World.MaxCoords.Y - 1);
+                x.location.X = Math.Min(Math.Max(x.location.X, 1),  World.MaxCoords.X - 1);
+            });
         }
     }
 }
